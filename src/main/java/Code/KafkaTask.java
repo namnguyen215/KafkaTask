b5b4297a93a21901ae49ef9ec17acf3ba506c7b2
namnguyen215 <namnguyenphuong215@gmail.com>
2022-05-26 15:11:50 +0700

    kafka

--- src/main/java/Code/KafkaTask.java
+++ src/main/java/Code/KafkaTask.java
@@ -3,11 +3,8 @@ package Code;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.streaming.StreamingQueryException;
 
-
-import java.util.concurrent.TimeoutException;
-
+import static org.apache.spark.sql.functions.count;
 import static org.apache.spark.sql.functions.col;
 import static org.apache.spark.sql.functions.split;
 public class KafkaTask {
@@ -27,8 +24,10 @@ public class KafkaTask {
                 split(col("value"),"\t").getItem(6).cast("String").as("guid"),
                 split(col("value"),"\t").getItem(4).cast("String").as("bannerId"));
 //        value=value.filter("time >= '2022-05-24 06:00:00' and time <= '2022-05-25 06:00:00'");
-        value=value.filter("time >= '2022-05-24 06:00:00'");
+        value=value.filter("time >= '2022-05-24 06:00:00' && time <= '2022-05-26 06:00:00'");
         value.show(false);
+        Dataset<Row> res=value.groupBy(col("bannerId")).agg(count("guid"));
+        res.show(false);
 //        try {
 //            value.writeStream().format("console").outputMode("append").start().awaitTermination();
 //        } catch (StreamingQueryException e) {
