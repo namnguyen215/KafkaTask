4a3547914ebe1152eefe8bbfc41f88685341ae56
namnguyen215 <namnguyenphuong215@gmail.com>
2022-05-26 14:52:38 +0700

    kafka

--- src/main/java/Code/KafkaTask.java
+++ src/main/java/Code/KafkaTask.java
@@ -15,21 +15,21 @@ public class KafkaTask {
         SparkSession spark = SparkSession.builder().master("yarn").getOrCreate();
         spark.sparkContext().setLogLevel("ERROR");
         Dataset<Row> df = spark
-                .readStream()
+                .read()
                 .format("kafka")
                 .option("kafka.bootstrap.servers", "10.3.68.20:9092, 10.3.68.21:9092, 10.3.68.23:9092, 10.3.68.26:9092, 10.3.68.28:9092, 10.3.68.32:9092, 10.3.68.47:9092, 10.3.68.48:9092, 10.3.68.50:9092, 10.3.68.52:9092")
                 .option("subscribe", "rt-queue_1")
                 .option("startingOffsets", "earliest")
                 .load();
-        Dataset<Row> res=df.selectExpr("CAST(value AS STRING)");
+        Dataset<Row> value=df.selectExpr("CAST(value AS STRING)");
 
-        res=res.select(split(col("value"),"\t").getItem(0).cast("long").cast("timestamp").as("time"),
+        value=value.select(split(col("value"),"\t").getItem(0).cast("long").cast("timestamp").as("time"),
                 split(col("value"),"\t").getItem(6).cast("String").as("guid"),
                 split(col("value"),"\t").getItem(4).cast("String").as("bannerId"));
-//        res=res.filter("time >= '2022-05-24 06:00:00' and time <= '2022-05-25 06:00:00'");
-        res=res.filter("time >= '2022-05-24 06:00:00'");
+//        value=value.filter("time >= '2022-05-24 06:00:00' and time <= '2022-05-25 06:00:00'");
+        value=value.filter("time >= '2022-05-24 06:00:00'");
         try {
-            res.writeStream().format("console").outputMode("append").start().awaitTermination();
+            value.writeStream().format("console").outputMode("append").start().awaitTermination();
         } catch (StreamingQueryException e) {
             throw new RuntimeException(e);
         } catch (TimeoutException e) {
