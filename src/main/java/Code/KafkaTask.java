51579567e2e8dc5cc0b53146c84772e5336c9143
namnguyen215 <namnguyenphuong215@gmail.com>
2022-05-26 14:34:35 +0700

    kafka

--- src/main/java/Code/KafkaTask.java
+++ src/main/java/Code/KafkaTask.java
@@ -19,6 +19,7 @@ public class KafkaTask {
                 .format("kafka")
                 .option("kafka.bootstrap.servers", "10.3.68.20:9092, 10.3.68.21:9092, 10.3.68.23:9092, 10.3.68.26:9092, 10.3.68.28:9092, 10.3.68.32:9092, 10.3.68.47:9092, 10.3.68.48:9092, 10.3.68.50:9092, 10.3.68.52:9092")
                 .option("subscribe", "rt-queue_1")
+                .option("startingOffsets", "earliest")
                 .load();
         Dataset<Row> res=df.selectExpr("CAST(value AS STRING)");
 
@@ -26,7 +27,7 @@ public class KafkaTask {
                 split(col("value"),"\t").getItem(6).cast("String").as("guid"),
                 split(col("value"),"\t").getItem(4).cast("String").as("bannerId"));
 //        res=res.filter("time >= '2022-05-24 06:00:00' and time <= '2022-05-25 06:00:00'");
-        res=res.filter("time >= '2022-05-24 06:00:00'");
+        res=res.filter("time >= '2022-05-24 06:00:00'").filter("time <= '2022-05-26 06:00:00'");
         try {
             res.writeStream().format("console").outputMode("append").start().awaitTermination();
         } catch (StreamingQueryException e) {
