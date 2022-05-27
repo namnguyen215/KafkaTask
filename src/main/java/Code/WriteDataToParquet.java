3f66cb65d439cf501f5163f6efc61f3ac26c727a
namnguyen215 <namnguyenphuong215@gmail.com>
2022-05-27 14:48:02 +0700

    kafka

--- src/main/java/Code/WriteDataToParquet.java
+++ src/main/java/Code/WriteDataToParquet.java
@@ -3,6 +3,9 @@ package Code;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.streaming.StreamingQueryException;
+
+import java.util.concurrent.TimeoutException;
 
 import static org.apache.spark.sql.functions.col;
 import static org.apache.spark.sql.functions.split;
@@ -24,6 +27,7 @@ public class WriteDataToParquet {
                 .option("kafka.bootstrap.servers", "10.3.68.20:9092, 10.3.68.21:9092, 10.3.68.23:9092, 10.3.68.26:9092, 10.3.68.28:9092, 10.3.68.32:9092, 10.3.68.47:9092, 10.3.68.48:9092, 10.3.68.50:9092, 10.3.68.52:9092")
                 .option("subscribe", "rt-queue_1")
                 .option("startingOffsets", "earliest")
+                .option("endingOffsets", "latest")
                 .load();
         Dataset<Row> value=df.selectExpr("CAST(value AS STRING)");
         value=value.select(split(col("value"),"\t").getItem(0).cast("long").cast("timestamp").as("time"),
@@ -33,7 +37,13 @@ public class WriteDataToParquet {
                 .withColumn("Hour",split(col("time")," ").getItem(1))
                 .drop(col("time"));
 
-        value.writeStream().format("parquet").option("path","hdfs:/mydata");
+        try {
+            value.writeStream().format("parquet").option("path","hdfs:/mydata").start().awaitTermination();
+        } catch (StreamingQueryException e) {
+            throw new RuntimeException(e);
+        } catch (TimeoutException e) {
+            throw new RuntimeException(e);
+        }
 //        value=value.withColumn("Day",when((col("Hour")==="06:00:00"),lit("new Day")))
 
 //        value.withColumn("Day",when());
