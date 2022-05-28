687bed48837a9caee8789f2e0ef1a79f4ce7595e
namnguyen215 <namnguyenphuong215@gmail.com>
2022-05-28 11:42:57 +0700

    kafka

---
+++ src/main/java/code/GetResult.java
@@ -0,0 +1,33 @@
+package code;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import static com.swoop.alchemy.spark.expressions.hll.functions.hll_cardinality;
+import static com.swoop.alchemy.spark.expressions.hll.functions.hll_init_agg;
+import static org.apache.spark.sql.functions.col;
+
+public class GetResult {
+
+    private static  String hdfsPath = "hdfs://internship-hadoop105185:8120/mydata/";
+    private static String date = "2022-05-28";
+    private static String path = hdfsPath + "Date=" + date + "/*";
+    /*
+    *Ham main
+    */
+    public static void main(final String[] args) {
+        SparkSession spark = SparkSession
+                .builder()
+                .master("yarn")
+                .getOrCreate();
+        spark.sparkContext().setLogLevel("ERROR");
+        Dataset<Row> df = spark.read().parquet(path);
+        Dataset<Row> res = df.groupBy(col("bannerId"))
+                .agg(hll_init_agg("guid")
+                        .as("guid_hll"));
+        res.select(col("bannerId"),
+                        hll_cardinality("guid_hll"))
+                .show(false);
+    }
+}
