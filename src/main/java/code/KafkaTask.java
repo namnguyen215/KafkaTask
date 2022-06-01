e66779ef933aaf4dc703e3ccddedfd509a1eb4b9
namnguyen215 <namnguyenphuong215@gmail.com>
2022-06-01 10:43:56 +0700

    kafka

--- src/main/java/code/KafkaTask.java
+++ src/main/java/code/KafkaTask.java
@@ -56,7 +56,7 @@ public class KafkaTask {
         value = value.groupBy(col("Day"), col("bannerId"))
                 .agg(hll_init_agg("guid")
                         .as("guid_hll"));
-        value = value.groupBy(col("bannerId"))
+        value = value.groupBy(col("Day"), col("bannerId"))
                 .agg(hll_merge("guid_hll")
                         .as("guid_hll"));
         try {
